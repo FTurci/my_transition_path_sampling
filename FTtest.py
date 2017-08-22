fc25df0fcf08d3ea3aa9c9bcc524654c9f5f10b5
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-08-22 19:24:09 +0200

    No wildcard tps import

--- FTtest.py
+++ FTtest.py
@@ -2,21 +2,21 @@
 
 """Sketch of TPS simulation."""
 
-import numpy
+import numpy as np
 from copy import copy
 from atooms.trajectory import TrajectoryRam, TrajectoryXYZ
 from atooms.simulation import Simulation
 from atooms.simulation.backends import DryRunBackend, LammpsBackend
 from atooms.utils import setup_logging
+import tps
 
-from tps import *
 
 # Parameters
 n = 1
 slices = 2
 steps = 1
 file_inp = 'lj_rho1.0.xyz'
-setup_logging(level=20)  # 20 is verbose, 40 just warnings
+setup_logging(level=10)  # 20 is verbose, 40 just warnings
 
 # Set up data structures
 # Replicas of simulations (each simulation might have different parameters)
@@ -57,13 +57,13 @@ for i in range(len(sim)):
 # TPS simulation
 tps_steps = 20
 # calculating the value of the initial potential
-setup(bias, sim,trj,umbrellas)
+tps.setup(bias, sim,trj,umbrellas)
 
 print "bias after initialisation",bias
 for k in range(tps_steps):
-    print "TPS step",k
+    tps._log.info('tps step %s', k)
     # We might have several replicas of simulations with different parameters
     for i in range(len(sim)): #FT: to be distributed?
-        mc_step(sim[i], trj[i], umbrellas[i], bias[i])
+        tps.mc_step(sim[i], trj[i], umbrellas[i], bias[i])
 
 
