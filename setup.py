4426863dbd56b93def995e87c4937b4326437cf8
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-08-24 17:39:33 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ except ImportError:
 with open('README.md') as f:
     readme = f.read()
 
-setup(name='sample',
+setup(name='transition_path_sampling',
       version='0.1',
       description='',
       long_description=readme,
       author='Daniele Coslovich',
       author_email='daniele.coslovich@umontpellier.fr',
-      url='https://gitlab.info-ufr.univ-montp2.fr/daniele.coslovich/sample',
+      url='https://gitlab.info-ufr.univ-montp2.fr/daniele.coslovich/atooms/transition_path_sampling',
       packages=[],
       #packages=find_packages(exclude=('tests', 'docs')),
       license='GPLv3',
